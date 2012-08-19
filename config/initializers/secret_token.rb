5cdb7b3b5a763724401b36a8434b7a3e5e1c8c84
jarode <jarode2@o2.pl>
2012-08-19 19:20:09 +0100

    .

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Auction22::Application.config.secret_token = 'f7ed8933f4a9b51006d9731a8f801de24352c26588bc653a525ff055b9c6edf622e58cbebc1dadb1df4e93963bc8c46778dda99e0c3f3ebec051e8f31ed17e93'
